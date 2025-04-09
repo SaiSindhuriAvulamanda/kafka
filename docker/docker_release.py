6d80a9f46d74c1336d289935f67bea00f4b8e15a
Sai <128015371+SaiSindhuriAvulamanda@users.noreply.github.com>
2025-04-09 16:21:23 +0530

    Update docker_release.py

--- docker/docker_release.py
+++ docker/docker_release.py
@@ -44,7 +44,7 @@ def build_push(image, kafka_url, image_type):
     try:
         create_builder()
         build_docker_image_runner(f"docker buildx build -f $DOCKER_FILE --build-arg kafka_url={kafka_url} --build-arg build_date={date.today()} --push \
-              --platform linux/amd64,linux/arm64 --tag {image} $DOCKER_DIR", image_type)
+              --platform linux/s390x --tag {image} $DOCKER_DIR", image_type)
     except:
         raise SystemError("Docker image push failed")
     finally:
